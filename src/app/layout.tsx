77a6568ca1bd181daf2a1f81c7c6e788d0c703b8
ahamedamansoor.dev <ahamedamansoor.dev@gmail.com>
2025-11-25 16:37:02 +0000

    added notes page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
- 
+
 import { cn } from "@/lib/utils"
 import "./globals.css";
 import { Providers } from './providers';
- 
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
